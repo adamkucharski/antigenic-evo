ed3fc94b139c4195f0cf666c6da278138b25968a
adamkucharski <adamkuchars@adams-imac.lshtm.ac.uk>
2016-11-17 15:01:31 +0000

    update map plots

--- immunity_model/main_do_antigenic.R
+++ immunity_model/main_do_antigenic.R
@@ -17,7 +17,7 @@ source("model_functions.R")  #Set up functions
 dataload="Fluscape_08"
 #dataload="Australia_98"
 
-landscape.build(dataload,d.step=0.5,extendD=5,bandW=22) # Generate maps
+landscape.build(dataload,d.step=0.25,extendD=5,bandW=22) # Generate maps
 
 # Plot data
 landscape.plot(dataload,radius1=5,yearload=2009,groupN=2)
