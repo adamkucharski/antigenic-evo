27819771ef41ff9a98ade01a0564e1dab32da25f
adamkucharski <adam.kucharski@lshtm.ac.uk>
2017-02-07 18:41:23 +0000

    plot for paper

--- immunity_model/main_do_antigenic.R
+++ immunity_model/main_do_antigenic.R
@@ -20,9 +20,9 @@ dataload="Fluscape_08"
 landscape.build(dataload,d.step=0.25,extendD=5,bandW=22) # Generate maps
 
 # Plot data
-landscape.plot(dataload,radius1=5,yearload=2009,groupN=2)
+landscape.plot(dataload,radius1=5,yearload=2009,groupN=2,borderA=F)
 
-reproduction.number.plot(dataload,rR=2)
+reproduction.number.plot(dataload,rR=2,borderA=F)
 
 #titre.plot(dataload)
 #proct.plot(dataload)
